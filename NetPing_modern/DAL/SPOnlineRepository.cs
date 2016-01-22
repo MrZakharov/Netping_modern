1cdb0454632336e0eb97ee8faa80009606b295af
smakarenko <innovaciiv@gmail.com>
2016-01-22 16:53:42 +1000

    fix

--- NetPing_modern/DAL/SPOnlineRepository.cs
+++ NetPing_modern/DAL/SPOnlineRepository.cs
@@ -146,8 +146,6 @@ namespace NetPing.DAL
         }
 
 
-
-
         public IEnumerable<Device> Devices
         {
             get
@@ -913,7 +911,7 @@ namespace NetPing.DAL
                     termsLabels = TermsLabels;
                     devices = Devices_Read(posts, sFiles, devicePhotos, devicesParameters, terms, termsDestinations, termsLabels);
                     HttpRuntime.Cache.Insert("Devices", devices);
-                    //PushToCache("Devices", devices);
+                    PushToCache("Devices", devices);
                     break;
                 case "GenerateYml":
                     if (Helpers.IsCultureRus)
@@ -935,7 +933,7 @@ namespace NetPing.DAL
                     PushToCache("PubFiles", PubFiles);
                     PushToCache("SFiles", SFiles);
                     PushToCache("Posts", Posts);
-                    PushToCache("Devices", Devices);
+                    //PushToCache("Devices", Devices);
                     break;
                 case "OnlyDevices":
 
